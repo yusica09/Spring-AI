efe734a855421b0d31b47a89b5ebf836fd6a2e4f
yusica09 <choijiwoo0922@gmail.com>
2026-07-27 19:33:26 +0900

    spring ai advisor

---
+++ spring-ai-advisor/src/main/java/com/example/demo/advisor/AdvisorC.java
@@ -0,0 +1,41 @@
+package com.example.demo.advisor;
+
+import org.springframework.ai.chat.client.ChatClientRequest;
+import org.springframework.ai.chat.client.ChatClientResponse;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisor;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisorChain;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisor;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisorChain;
+import org.springframework.core.Ordered;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+public class AdvisorC implements CallAdvisor, StreamAdvisor {
+	@Override
+    public String getName() { 
+		return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public int getOrder() { 
+    	return Ordered.HIGHEST_PRECEDENCE + 3;
+    }
+
+    @Override
+    public ChatClientResponse adviseCall(ChatClientRequest request, CallAdvisorChain chain) {
+      log.info("[전처리]");
+      ChatClientResponse advisedResponse = chain.nextCall(request);
+      log.info("[후처리]");
+      return advisedResponse;
+    }
+
+    @Override
+    public Flux<ChatClientResponse> adviseStream(ChatClientRequest request, StreamAdvisorChain chain) {
+      log.info("[전처리]");
+      Flux<ChatClientResponse> flux = chain.nextStream(request); 
+      flux.blockLast();
+      return flux; 
+    }
+}
